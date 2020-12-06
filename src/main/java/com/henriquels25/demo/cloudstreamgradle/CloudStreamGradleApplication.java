708d024c557091652fd8b7fcce8626014a59838c
Henrique Schmidt <henriquels25@gmail.com>
2020-12-06 14:03:15 -0300

    Initial commit

---
+++ src/main/java/com/henriquels25/demo/cloudstreamgradle/CloudStreamGradleApplication.java
@@ -0,0 +1,13 @@
+package com.henriquels25.demo.cloudstreamgradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudStreamGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudStreamGradleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/henriquels25/demo/cloudstreamgradle/CloudStreamGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.henriquels25.demo.cloudstreamgradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudStreamGradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
